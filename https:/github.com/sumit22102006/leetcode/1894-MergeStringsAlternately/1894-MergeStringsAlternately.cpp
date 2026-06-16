e84b6c413124fd18e6f735271847d978102f6518
Sumit kumar <sumitkryaduvanshi@gmail.com>
2026-06-16 13:03:04 +0530

    Create 1894-MergeStringsAlternately

---
+++ https:/github.com/sumit22102006/leetcode/1894-MergeStringsAlternately/1894-MergeStringsAlternately.cpp
@@ -0,0 +1,25 @@
+// Last updated: 16/06/2026, 13:03:03
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+
+   int i = 0;
+   int j = 0;
+   string ans = "";
+
+   while(i<word1.length() || j<word2.length()){
+    if(i<word1.length()){
+        ans +=word1[i];
+        ++i;
+    }
+
+    if(j<word2.length()){
+        ans += word2[j];
+        ++j;
+    }
+   }
+
+     return ans;
+      
+    }
+};

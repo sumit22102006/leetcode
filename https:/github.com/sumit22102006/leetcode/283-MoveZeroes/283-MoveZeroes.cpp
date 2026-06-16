f3d7bbcf55b2591cf49cb4e0bc9eb9384173abf4
Sumit kumar <sumitkryaduvanshi@gmail.com>
2026-06-16 13:03:52 +0530

    Create 283-MoveZeroes

---
+++ https:/github.com/sumit22102006/leetcode/283-MoveZeroes/283-MoveZeroes.cpp
@@ -0,0 +1,16 @@
+// Last updated: 16/06/2026, 13:03:51
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+
+        int j=0;
+        for(int i =0 ; i<nums.size(); ++i){
+
+            if(nums[i]!=0){
+                swap(nums[i], nums[j]);
+                ++j;
+            }
+        }
+        
+    }
+};

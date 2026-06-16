97d144dd66eacc2e54262d878c2317da8f657287
Sumit kumar <sumitkryaduvanshi@gmail.com>
2026-06-16 13:03:23 +0530

    Create 1044-FindCommonCharacters

---
+++ https:/github.com/sumit22102006/leetcode/1044-FindCommonCharacters/1044-FindCommonCharacters.cpp
@@ -0,0 +1,23 @@
+// Last updated: 16/06/2026, 13:03:21
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+        vector <string> res={};
+        for(char i : words[0]){
+            string temp="";
+            temp+=i;
+            bool found=true;
+            for(int j=1;j<words.size();j++){
+                if(!words[j].contains(i)){
+                    found=false;
+                    break;
+                }
+                words[j].erase(words[j].find(i),1);
+            }
+            if(found){
+                res.push_back(temp);
+            }
+        }
+        return res;
+    }
+};

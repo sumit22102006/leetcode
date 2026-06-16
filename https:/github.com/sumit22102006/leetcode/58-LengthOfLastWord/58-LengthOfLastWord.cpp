6f6019ae9a3978c080e2da9caa7c7d00953f97cf
Sumit kumar <sumitkryaduvanshi@gmail.com>
2026-06-16 13:04:01 +0530

    Create 58-LengthOfLastWord

---
+++ https:/github.com/sumit22102006/leetcode/58-LengthOfLastWord/58-LengthOfLastWord.cpp
@@ -0,0 +1,20 @@
+// Last updated: 16/06/2026, 13:03:59
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        
+     int i= s.length()-1;
+     int length =0;
+     while (i>=0 && s[i]==' '){
+        --i;
+     }
+     while(i>=0 &&   s[i]!=' '   ){
+        ++length;
+        --i;
+     }
+
+    
+
+return length;
+    }
+};

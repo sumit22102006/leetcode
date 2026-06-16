5424228c7f2981ab1b94525ad7d3adb99d720c35
Sumit kumar <sumitkryaduvanshi@gmail.com>
2026-06-16 13:03:16 +0530

    Create 2383-AddTwoIntegers

---
+++ https:/github.com/sumit22102006/leetcode/2383-AddTwoIntegers/2383-AddTwoIntegers.cpp
@@ -0,0 +1,11 @@
+// Last updated: 16/06/2026, 13:03:15
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        int result = 0;
+    result= num1+num2;
+
+    return result;
+        
+    }
+};

998036a9d68f305aafb857e56a9679795af34690
Sumit kumar <sumitkryaduvanshi@gmail.com>
2026-06-17 09:11:31 +0530

    Create 28-FindTheIndexOfTheFirstOccurrenceInAString

---
+++ https:/github.com/sumit22102006/leetcode.git/28-FindTheIndexOfTheFirstOccurrenceInAString/28-FindTheIndexOfTheFirstOccurrenceInAString.cpp
@@ -0,0 +1,24 @@
+// Last updated: 17/06/2026, 09:11:29
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+
+        int n = haystack.size();
+        int m = needle.size();
+
+        for(int i = 0; i <= n - m; i++) {
+
+            int j = 0;
+
+            while(j < m && haystack[i + j] == needle[j]) {
+                j++;
+            }
+
+            if(j == m) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};

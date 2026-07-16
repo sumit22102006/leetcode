0765a5ff4384e8182ca204f6924811e83d8a41e9
Sumit kumar <sumitkryaduvanshi@gmail.com>
2026-07-16 20:56:01 +0530

    Create 347-TopKFrequentElements

---
+++ https:/github.com/sumit22102006/leetcode.git/347-TopKFrequentElements/347-TopKFrequentElements.cpp
@@ -0,0 +1,28 @@
+// Last updated: 16/07/2026, 20:55:58
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+
+        
+        unordered_map<int, int> freq;
+
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        vector<pair<int, int>> v(freq.begin(), freq.end());
+
+        sort(v.begin(), v.end(), [](pair<int, int> a, pair<int, int> b) {
+            return a.second > b.second;
+        });
+
+        vector<int> ans;
+
+        for (int i = 0; i < k; i++) {
+            ans.push_back(v[i].first);
+        }
+
+        return ans;
+        
+    }
+};

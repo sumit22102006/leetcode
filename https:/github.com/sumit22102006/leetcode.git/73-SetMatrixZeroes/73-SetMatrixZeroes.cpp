8ba7ad654da10b0a8f973aaeb2c840e03eeafda0
Sumit kumar <sumitkryaduvanshi@gmail.com>
2026-07-18 23:46:59 +0530

    Create 73-SetMatrixZeroes

---
+++ https:/github.com/sumit22102006/leetcode.git/73-SetMatrixZeroes/73-SetMatrixZeroes.cpp
@@ -0,0 +1,30 @@
+// Last updated: 18/07/2026, 23:46:58
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        vector<int> row(rows, 0);
+         vector<int> col(cols, 0);
+
+        for(int i =0; i<rows ; i++){
+            for(int j=0 ; j<cols ; j++){
+                if(matrix[i][j]==0){
+                   row[i] = 1;
+                   col[j] = 1;
+                }
+               
+            }
+        }
+
+        for(int i = 0; i < rows; i++) {
+    for(int j = 0; j < cols; j++) {
+        if(row[i] == 1 || col[j] == 1) {
+            matrix[i][j] = 0;
+        }
+    }
+}
+
+        return ;
+    }
+};

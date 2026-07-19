d717dfdac88884b97fd4b96dbc991308a450d0df
Sumit kumar <sumitkryaduvanshi@gmail.com>
2026-07-19 21:49:02 +0530

    Create 80-RemoveDuplicatesFromSortedArrayIi

---
+++ https:/github.com/sumit22102006/leetcode.git/80-RemoveDuplicatesFromSortedArrayIi/80-RemoveDuplicatesFromSortedArrayIi.cpp
@@ -0,0 +1,23 @@
+// Last updated: 19/07/2026, 21:49:01
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+
+        int n = nums.size();
+
+        if (n <= 2)
+            return n;
+
+        int i = 2;
+
+        for (int j = 2; j < n; j++) {
+
+            if (nums[j] != nums[i - 2]) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+
+        return i;
+    }
+};

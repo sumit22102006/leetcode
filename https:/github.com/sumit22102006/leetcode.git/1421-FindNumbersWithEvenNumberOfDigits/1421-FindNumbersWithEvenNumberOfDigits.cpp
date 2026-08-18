fc0a6d9f7deabdea003a22c02b44ff05c264d66b
Sumit kumar <sumitkryaduvanshi@gmail.com>
2026-08-18 14:40:03 +0530

    Create 1421-FindNumbersWithEvenNumberOfDigits

---
+++ https:/github.com/sumit22102006/leetcode.git/1421-FindNumbersWithEvenNumberOfDigits/1421-FindNumbersWithEvenNumberOfDigits.cpp
@@ -0,0 +1,21 @@
+// Last updated: 18/08/2026, 14:40:03
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int result = 0 ;
+       
+
+        for(int i = 0 ;  i <nums.size() ; i++){
+             int count = 0;
+            while(nums[i]>0){
+                nums[i]%10;
+           nums[i]= nums[i]/10;
+                count++ ;
+            }
+            if(count%2==0){
+                result++;
+            }
+        }
+        return result;
+    }
+};

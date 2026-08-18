cc35e69e3ed18044c5c76b510f62d2cc04987750
Sumit kumar <sumitkryaduvanshi@gmail.com>
2026-08-18 14:39:45 +0530

    Create 1791-RichestCustomerWealth

---
+++ https:/github.com/sumit22102006/leetcode.git/1791-RichestCustomerWealth/1791-RichestCustomerWealth.cpp
@@ -0,0 +1,16 @@
+// Last updated: 18/08/2026, 14:39:45
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        vector<int>v;
+        for(int i=0 ; i<accounts.size();i++){
+            int sum =0;
+            for(int j=0 ; j<accounts[i].size() ; j++){
+                sum = sum +accounts[i][j];
+                
+            }
+             v.push_back(sum);
+        }
+        return *max_element(v.begin(), v.end());
+    }
+};

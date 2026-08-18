eb1615aa1d819756e7d80d682868ae7517a31c56
Sumit kumar <sumitkryaduvanshi@gmail.com>
2026-08-18 14:39:22 +0530

    Create 2556-ConvertTheTemperature

---
+++ https:/github.com/sumit22102006/leetcode.git/2556-ConvertTheTemperature/2556-ConvertTheTemperature.cpp
@@ -0,0 +1,13 @@
+// Last updated: 18/08/2026, 14:39:21
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        
+        vector<double>v;
+
+        v.push_back(celsius+273.15);
+        v.push_back(celsius*1.8 +32.00);
+
+        return v;
+    }
+};

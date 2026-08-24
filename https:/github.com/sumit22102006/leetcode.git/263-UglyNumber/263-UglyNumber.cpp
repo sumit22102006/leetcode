e5d1de45f401a674f69819d78d35b40cf54e6b42
Sumit kumar <sumitkryaduvanshi@gmail.com>
2026-08-24 13:26:35 +0530

    Create 263-UglyNumber

---
+++ https:/github.com/sumit22102006/leetcode.git/263-UglyNumber/263-UglyNumber.cpp
@@ -0,0 +1,22 @@
+// Last updated: 24/08/2026, 13:26:35
+class Solution {
+public:
+    bool isUgly(int n) {
+       if(n<= 0){
+        return false;
+       }
+
+       while(n%2==0){
+         n /=2;
+       }
+       while(n%3==0){
+        n /=3;
+       }
+
+       while(n%5==0){
+        n /=5;
+       }
+       
+       return n==1;
+    }
+};

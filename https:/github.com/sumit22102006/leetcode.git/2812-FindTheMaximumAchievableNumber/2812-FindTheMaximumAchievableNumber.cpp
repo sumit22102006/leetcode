3b6300c2d7bda533edcd6cd0e913560a8a2de2ae
Sumit kumar <sumitkryaduvanshi@gmail.com>
2026-08-25 11:52:56 +0530

    Create 2812-FindTheMaximumAchievableNumber

---
+++ https:/github.com/sumit22102006/leetcode.git/2812-FindTheMaximumAchievableNumber/2812-FindTheMaximumAchievableNumber.cpp
@@ -0,0 +1,9 @@
+// Last updated: 25/08/2026, 11:52:55
+class Solution {
+public:
+    int theMaximumAchievableX(int num, int t) {
+        int maximum = num + t + t;
+
+        return maximum ;
+    }
+};
